064bf129513b4d2d28778e76fa8670bc41d0e177
anmelnik <melnikvanastasia@gmail.com>
2020-10-20 21:56:40 +0300

    reiteration

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-// function greet (name) {
-//     console.log ("Hello,", name)
-// }
-// greet("Elena")
-//
-// const t = function t (name) {
-//     console.log("Hello -", name)
-// }
-// t("Anna")
+// create array with length = 10 start position letter a e.g [a, b, c ... ]
+// print all elements from array using while loop
+// if array eleemnt equal 'c' or 'g', print it
 
-// let counter = 0
-// const gg = setInterval(function () {
-//     if (counter === 7) {
-//         clearInterval(gg)
-//     } else {
-//         console.log(++counter)
-//     }
-// }, 100)
-//
-// const arrow = name => console.log("Hello,", name)
-// arrow("Lena")
-//
-// const pow = num => num**8
-// console.log(pow(5))
+const user = {
+    name: "Vasya",
+    age: 10,
+    sex: "male",
+    maried: true,
+    lovers: ['Tanka', 'Olka', 'Yulka', 'Mashka']
+}
 
-// const num = (a, b, c) => a + b * c
-// console.log(num(4, 5, 2))
-//
-// function sumAll(...all) {
-//     let result = 0
-//     for (let num of all) {
-//         result  += num
-//     }
-//     return result
-// }
-// const ref = sumAll(1,3,5,6,8)
-//  console.log(ref)
-
-// function member (name) {
-//     return function (lastName) {
-//         console.log(name + lastName)
-//     }
-// }
-// const ref = member ("Yliya ")
-// console.log(ref("Melnik"))
+let i = 0
+let keys = Object.keys(user)
+let j = keys.length
+while (i < j) {
+    if (keys[i] === "lovers") {
+        let lovers = user[keys[i]]
+        let y = 0
+        while (y < lovers.length) {
+            console.log(lovers[y])
+            y++
+        }
+    }
+    i++
+}
