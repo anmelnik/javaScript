44ce943d22f3473aed6b49bb5c837b892c908648
anmelnik <melnikvanastasia@gmail.com>
2020-10-21 20:02:02 +0300

    study array, function, object

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-// create array with length = 10 start position letter a e.g [a, b, c ... ]
-// print all elements from array using while loop
-// if array eleemnt equal 'c' or 'g', print it
-
-const user = {
-    name: "Vasya",
-    age: 10,
-    sex: "male",
-    maried: true,
-    lovers: ['Tanka', 'Olka', 'Yulka', 'Mashka']
+const person = {
+    name: "Vlad",
+    lastName: "Kuznitzov",
+    age: undefined,
+    // ['key_' + (1 + 5)]: "6",
+    languages: ["Lu", "Ua"],
+    // greet() {
+    //     console.log('greet from person')
+    // }
 }
 
-let i = 0
-let keys = Object.keys(user)
-let j = keys.length
-while (i < j) {
-    if (keys[i] === "lovers") {
-        let lovers = user[keys[i]]
-        let y = 0
-        while (y < lovers.length) {
-            console.log(lovers[y])
-            y++
-        }
+// person['key_6'] = "undefind"
+// person.age++
+// delete person.age
+// console.log(person)
+
+// const {name, lastName, age: personAge = 21} = person 
+// console.log(name, lastName, personAge)
+
+for (let key in person) {
+    if( person.hasOwnProperty(key)) {
+        console.log(key)
+        console.log(person[key])
     }
-    i++
 }
